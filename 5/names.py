fdbcd04e8296e67f55d613c7e236ad55caf2f13f
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-05-15 15:03:43 +0200

    submission Bite 5 @ codechalleng.es

--- 5/names.py
+++ 5/names.py
@@ -18,13 +18,20 @@ NAMES = [
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
     each name appears only once"""
-    pass
+    title_cased_names = [name.title() for name in names]
+
+    deduped_names = []
+    for name in title_cased_names:
+        if name not in deduped_names:
+            deduped_names.append(name)
+
+    return deduped_names
 
 
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    # ...
+    return sorted(names, key=lambda x: x.split(" ")[-1], reverse=True)
 
 
 def shortest_first_name(names):
@@ -32,4 +39,5 @@ def shortest_first_name(names):
     You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
-    # ...
+    first_names = [x.split()[0] for x in names]
+    return min(first_names, key=len)
