e2e258802762506044f4f8c37a4e8c34d5eb361f
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-05-15 15:25:52 +0200

    submission Bite 8 @ codechalleng.es

--- 8/rotate.py
+++ 8/rotate.py
@@ -1,5 +1,13 @@
+from collections import deque
+
+
 def rotate(string, n):
     """Rotate characters in a string.
     Expects string and n (int) for number of characters to move.
     """
-    pass
+    deq = deque(string)
+    if n > 0:
+        deq.rotate(-n)
+    else:
+        deq.rotate(abs(n))
+    return "".join(deq)
