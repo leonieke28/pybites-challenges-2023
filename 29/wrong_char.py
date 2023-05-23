f14ec327aaa531a9464650d7391e04a27794d0d2
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-05-23 05:01:47 +0200

    submission Bite 29 @ codechalleng.es

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,13 @@
 def get_index_different_char(chars):
-    pass
+    alphanums = []
+    non_alphanums = []
+    for i, char in enumerate(chars):
+        if str(char).isalnum():
+            alphanums.append((i, char))
+        else:
+            non_alphanums.append((i, char))
+
+    if len(alphanums) > len(non_alphanums):
+        return non_alphanums[0][0]
+    else:
+        return alphanums[0][0]
