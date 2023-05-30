fb89c9e2d088b959fdd965f550d9553b05b80cb0
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-05-30 06:27:28 +0200

    submission Bite 46 @ codechalleng.es

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -2,4 +2,6 @@ from typing import Union
 
 
 def fizzbuzz(num: int) -> Union[str, int]:
-    pass
+    if num % 5 == 0:
+        return "Fizz Buzz" if num % 3 == 0 else "Buzz"
+    return "Fizz" if num % 3 == 0 else num
