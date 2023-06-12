362895f5acb5a3812b353a08421ab1c965dec234
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-06-12 10:18:00 +0200

    submission Bite 64 @ codechalleng.es

--- 64/event.py
+++ 64/event.py
@@ -6,7 +6,9 @@ confirmed = [False, True, True, False, True]
 
 
 def get_attendees():
-    for participant in zip(names, locations, confirmed):
+    for participant in itertools.zip_longest(
+        names, locations, confirmed, fillvalue="-"
+    ):
         print(participant)
 
 
