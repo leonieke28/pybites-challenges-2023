3726d4dc9cc9be722ffe0f58adaffa976f307b45
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-06-14 10:11:16 +0200

    submission Bite 68 @ codechalleng.es

--- 68/clean.py
+++ 68/clean.py
@@ -1,3 +1,6 @@
+import string
+
+
 def remove_punctuation(input_string):
     """Return a str with punctuation chars stripped out"""
-    pass
+    return "".join("" if char in string.punctuation else char for char in input_string)
