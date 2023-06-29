60a0013e029dda3dde2efd52c054e3a037a1a3f8
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-06-29 15:07:57 +0200

    submission Bite 96 @ codechalleng.es

--- 96/wc.py
+++ 96/wc.py
@@ -3,7 +3,14 @@ def wc(file_):
     lines/words/chars, and returns a string of these numbers + file, e.g.:
     3 12 60 /tmp/somefile
     (both tabs and spaces are allowed as separator)"""
-    pass
+    with open(file_, "r") as f:
+        content = f.read()
+        num_lines = len(content.splitlines())
+        num_words = len(content.split())
+        num_chars = len(content)
+        result = f"{num_lines}\t{num_words}\t{num_chars}\t{file_}"
+
+    return result
 
 
 if __name__ == "__main__":
