08c9789b6cd9b9da38fd1021fbbd224dd3f85764
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-06-29 14:32:17 +0200

    submission Bite 54 @ codechalleng.es

--- 54/poem.py
+++ 54/poem.py
@@ -1,5 +1,18 @@
+import textwrap
+
 INDENTS = 4
 
 
 def print_hanging_indents(poem):
-    pass
+    formatted_poem = []
+    empty_line = False
+    for line in [line.strip() for line in poem.splitlines(True)]:
+        if line == "":
+            empty_line = True
+            continue
+        if not empty_line:
+            formatted_poem.append(textwrap.indent(line, " " * INDENTS))
+        else:
+            formatted_poem.append(line)
+        empty_line = False
+    print("\n".join(formatted_poem))
