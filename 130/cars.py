a79c03b6785bf626d1e81616492368db3a2e1991
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-03 08:39:11 +0200

    submission Bite 130 @ codechalleng.es

--- 130/cars.py
+++ 130/cars.py
@@ -9,15 +9,23 @@ CAR_DATA = "https://bites-data.s3.us-east-2.amazonaws.com/cars.json"
 with requests.Session() as s:
     data = s.get(CAR_DATA).json()
 
+print(data[:10])
+
 
 # your turn:
 def most_prolific_automaker(year):
-    """Given year 'year' return the automaker that released
+    """Given year 'year,' return the automaker that released
     the highest number of new car models"""
-    pass
+    automakers = [item["automaker"] for item in data if item["year"] == year]
+    return Counter(automakers).most_common(1)[0][0]
 
 
 def get_models(automaker, year):
-    """Filter cars 'data' by 'automaker' and 'year',
+    """Filter cars 'data' by 'automaker' and 'year,'
     return a set of models (a 'set' to avoid duplicate models)"""
-    pass
+    models = [
+        item["model"]
+        for item in data
+        if item["year"] == year and item["automaker"] == automaker
+    ]
+    return set(models)
