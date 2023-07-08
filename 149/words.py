c2885ae9c1031a1434ea5b31c8082247fdb97809
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-08 06:40:56 +0200

    submission Bite 149 @ codechalleng.es

--- 149/words.py
+++ 149/words.py
@@ -3,4 +3,16 @@ def sort_words_case_insensitively(words):
     (1995, 19ab = numbers / Happy, happy4you = strings, hence for
      numbers you only need to check the first char of the word)
     """
-    pass
+    words_starting_with_letter = []
+    words_starting_with_number = []
+
+    for word in words:
+        if word[0].isdigit():
+            words_starting_with_number.append(word)
+        else:
+            words_starting_with_letter.append(word)
+
+    words_starting_with_letter.sort(key=str.lower)
+    words_starting_with_number.sort(key=str.lower)
+
+    return words_starting_with_letter + words_starting_with_number
