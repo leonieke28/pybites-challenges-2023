3a668743e506807ae57acb9fca30868f371236e8
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-11 10:19:35 +0200

    submission Bite 169 @ codechalleng.es

--- 169/convert.py
+++ 169/convert.py
@@ -5,4 +5,10 @@ def convert(value: float, fmt: str) -> float:
     :param fmt: String indicating format to convert to
     :return: Float rounded to 4 decimal places after conversion
     """
-    pass
+    if not isinstance(value, (int, float)):
+        raise TypeError("Value must be an integer or a float")
+    fmt = fmt.lower()
+    if fmt in {"cm", "in"}:
+        return round(value * 2.54, 4) if fmt == "cm" else round(value / 2.54, 4)
+    else:
+        raise ValueError("fmt must be 'cm' or 'in'")
