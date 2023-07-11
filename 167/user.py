3532e1eaf7d8d4a7338c29ce610de8e85eecd8d4
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-11 09:47:38 +0200

    submission Bite 167 @ codechalleng.es

--- 167/user.py
+++ 167/user.py
@@ -14,7 +14,7 @@ class User:
         and using title case for both.
         """
         # TODO 1: you code
-        pass
+        return f"{self.first_name.title()} {self.last_name.title()}"
 
     @property
     def username(self):
@@ -26,7 +26,7 @@ class User:
         https://pybit.es/property-decorator.html
         """
         # TODO 2: you code
-        pass
+        return f"{self.first_name[0].lower()}{self.last_name[:7].lower()}"
 
     # TODO 3: you code
     #
@@ -37,10 +37,10 @@ class User:
     # see also TESTS for required output
 
     def __str__(self):
-        pass
+        return f"{self.first_name.title()} {self.last_name.title()} ({self.username})"
 
     def __repr__(self):
         """Don't hardcode the class name, hint: use a
         special attribute of self.__class__ ...
         """
-        pass
+        return f"{self.__class__.__name__}('{self.first_name}', '{self.last_name}')"
