b079041e7b79e598cff3beff4a0808fdafa55ad3
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-13 09:21:12 +0200

    submission Bite 176 @ codechalleng.es

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -4,4 +4,10 @@ WHITE, BLACK = " ", "#"
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
     Don't return anything, print the output to stdout"""
-    pass
+    for row in range(size):
+        for column in range(size):
+            if (row + column) % 2 == 0:
+                print(WHITE, end="")
+            else:
+                print(BLACK, end="")
+        print()
