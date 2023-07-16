cfa8924bc36613a544945a86dffde425828ddc8f
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-16 07:22:34 +0200

    submission Bite 189 @ codechalleng.es

--- 189/control_flow.py
+++ 189/control_flow.py
@@ -4,4 +4,15 @@ MAX_NAMES = 5
 
 
 def filter_names(names):
-    pass
+    result = []
+    for name in names:
+        if name.startswith(IGNORE_CHAR):
+            continue
+        elif any(char.isdigit() for char in name):
+            continue
+        elif name.startswith(QUIT_CHAR):
+            break
+        else:
+            result.append(name)
+
+    return result[:MAX_NAMES]
