6636251960e1f0db4891ca165f3885290265fd7a
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-16 10:10:33 +0200

    submission Bite 208 @ codechalleng.es

--- 208/combos.py
+++ 208/combos.py
@@ -1,2 +1,6 @@
+from itertools import combinations
+
+
 def find_number_pairs(numbers, N=10):
-    pass
+    number_pairs = list(combinations(numbers, 2))
+    return [pair for pair in number_pairs if pair[0] + pair[1] == N]
