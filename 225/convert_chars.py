7e463f5fe5161bb3612ff09b0b8ca07184f125f4
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-22 10:18:57 +0200

    submission Bite 225 @ codechalleng.es

--- 225/convert_chars.py
+++ 225/convert_chars.py
@@ -4,4 +4,6 @@ PYBITES = "pybites"
 def convert_pybites_chars(text):
     """Swap case all characters in the word pybites for the given text.
     Return the resulting string."""
-    pass
+    return "".join(
+        [char.swapcase() if char.lower() in PYBITES else char for char in text]
+    )
