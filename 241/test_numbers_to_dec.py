18104c5723be84325557498e2dc748b981b71e22
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-07-27 08:21:46 +0200

    submission Bite 241 @ codechalleng.es

--- 241/test_numbers_to_dec.py
+++ 241/test_numbers_to_dec.py
@@ -4,19 +4,33 @@ from numbers_to_dec import list_to_decimal
 
 
 @pytest.mark.parametrize(
-    "test_input, expected", [([0, 4, 2, 8], 428), ([1, 2], 12), ([3], 3)]
+    "test_input, expected",
+    [
+        ([0, 4, 2, 8], 428),
+        ([1, 2], 12),
+        ([3], 3),
+    ],
 )
-def test_list_to_decimal(test_input, expected):
-    assert list_to_decimal(test_input) == expected
+def test_valid_values(test_input, expected):
+    actual = list_to_decimal(test_input)
+    assert actual == expected
 
 
-@pytest.mark.parametrize("test_input", [[6, 2, True], [3.6, 4, 1], ["4", 5, 3, 1]])
-def test_type_error(test_input):
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        [6, 2, True],
+        [3.6, 4, 1],
+        ["4", 5, 3, 1],
+    ],
+)
+def test_invalid_input_types(test_input):
     with pytest.raises(TypeError):
-        output = list_to_decimal(test_input)
+        list_to_decimal(test_input)
 
 
-@pytest.mark.parametrize("test_input", [[-3, 5], [23, 11, 1]])
-def test_value_error(test_input):
+def test_invalid_range():
+    test_input = [-3, 12]
+
     with pytest.raises(ValueError):
-        output = list_to_decimal(test_input)
+        list_to_decimal(test_input)
