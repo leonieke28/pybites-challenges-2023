842cf78b2a41c4e76ab171ef6e86a155b2c5cd7a
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-02 14:37:52 +0200

    submission Bite 279 @ codechalleng.es

--- 279/armstrong.py
+++ 279/armstrong.py
@@ -1,2 +1,8 @@
 def is_armstrong(n: int) -> bool:
-    # your code ...
+    numbers = [int(x) for x in str(n)]
+
+    if len(numbers) == 1:
+        return True
+
+    result = [(number ** len(numbers)) for number in numbers]
+    return sum(result) == n
