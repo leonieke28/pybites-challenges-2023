4844d30f97d172ae9f6ebc041204e3c36d3ac0ad
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-02 14:10:07 +0200

    submission Bite 278 @ codechalleng.es

--- 278/frequency.py
+++ 278/frequency.py
@@ -1,9 +1,12 @@
+from collections import Counter
+
+
 def major_n_minor(numbers):
     """
     Input: an array with integer numbers
     Output: the majority and minority number
     """
-
-    # you code ...
+    major = Counter(numbers).most_common(1)[0][0]
+    minor = Counter(numbers).most_common()[: -1 - 1 : -1][0][0]
 
     return major, minor
