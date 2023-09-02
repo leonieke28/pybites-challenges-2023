90b05906acfc27d2bdef8309ec3d18ae9b9b17af
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-02 13:33:43 +0200

    submission Bite 262 @ codechalleng.es

--- 262/gc_content.py
+++ 262/gc_content.py
@@ -3,4 +3,11 @@ def calculate_gc_content(sequence):
     Receives a DNA sequence (A, G, C, or T)
     Returns the percentage of GC content (rounded to the last two digits)
     """
-    pass
+    a = sequence.lower().count("a")
+    g = sequence.lower().count("g")
+    c = sequence.lower().count("c")
+    t = sequence.lower().count("t")
+    total = a + g + c + t
+    gc_total = g + c
+
+    return round((gc_total / total) * 100, 2)
