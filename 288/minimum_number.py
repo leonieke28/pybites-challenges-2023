fa9fb0a606838b1cf9e7a5867aaac5da9926f799
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-03 13:32:03 +0200

    submission Bite 288 @ codechalleng.es

--- 288/minimum_number.py
+++ 288/minimum_number.py
@@ -2,4 +2,8 @@ from typing import List
 
 
 def minimum_number(digits: List[int]) -> int:
-    ...
+    if not digits:
+        return 0
+
+    digits_set = set(digits)
+    return int("".join(map(str, sorted(digits_set))))
