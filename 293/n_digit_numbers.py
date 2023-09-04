e6d2bfe495a38889d91db9a3c8308ad59d032826
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-04 14:21:04 +0200

    submission Bite 293 @ codechalleng.es

--- 293/n_digit_numbers.py
+++ 293/n_digit_numbers.py
@@ -4,4 +4,16 @@ T = TypeVar("T", int, float)
 
 
 def n_digit_numbers(numbers: List[T], n: int) -> List[int]:
-    ...
+    if n < 1:
+        raise ValueError("n should be greater than or equal to 1")
+
+    power_numbers = [str(number * (10 ** (n - 1))) for number in numbers]
+
+    n_digits = []
+    for power_number in power_numbers:
+        if power_number.startswith("-"):
+            n_digits.append(int(power_number[: n + 1]))
+        else:
+            n_digits.append(int(power_number[:n]))
+
+    return n_digits
