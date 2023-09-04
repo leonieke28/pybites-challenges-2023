b926e1329873078f05f5c62373aa7cf36275cbf5
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-04 14:41:28 +0200

    submission Bite 295 @ codechalleng.es

--- 295/join_lists.py
+++ 295/join_lists.py
@@ -2,4 +2,7 @@ from typing import List, Union
 
 
 def join_lists(lst_of_lst: List[List[str]], sep: str) -> Union[List[str], None]:
-    ...
+    if not lst_of_lst:
+        return None
+
+    return [lst for sublist in lst_of_lst for lst in sublist + [sep]][:-1]
