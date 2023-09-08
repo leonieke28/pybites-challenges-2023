f1ae6988d83689ad0aa0f02924befa0f0e1f7327
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-08 09:04:51 +0200

    submission Bite 314 @ codechalleng.es

--- 314/to_columns.py
+++ 314/to_columns.py
@@ -2,4 +2,9 @@ from typing import List  # not needed when we upgrade to 3.9
 
 
 def print_names_to_columns(names: List[str], cols: int = 2) -> None:
-    pass
+    for i, name in enumerate(names):
+        if i % cols == 0:
+            print("", end="")
+        print(f"| {name:<10}", end="")
+        if i % cols == cols - 1 or i == len(names) - 1:
+            print()
