e4b47286be9432234c94b4e1def544b22d06e548
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-09 11:24:47 +0200

    submission Bite 322 @ codechalleng.es

--- 322/progress.py
+++ 322/progress.py
@@ -2,4 +2,14 @@ from datetime import datetime
 
 
 def ontrack_reading(books_goal: int, books_read: int, day_of_year: int = None) -> bool:
-    pass
+    if day_of_year is None:
+        day_of_year = datetime.now().timetuple().tm_yday
+
+    days_past = (day_of_year / 365) * 100
+
+    should_be_read = (books_goal * days_past) / 100
+
+    if books_read >= should_be_read:
+        return True
+    else:
+        return False
