79c33ad7390ae66da6aa2237cc158a18a4f640fb
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-09-24 11:19:56 +0200

    submission Bite 360 @ codechalleng.es

--- 360/script.py
+++ 360/script.py
@@ -3,13 +3,16 @@ import time
 import typer
 from rich.progress import track
 
-
 app = typer.Typer()
 
 
 @app.command()
 def progress():
-    pass
+    total = 0
+    for _ in track(range(100), description="Processing..."):
+        time.sleep(0.1)
+        total += 1
+    print(f"Processed {total} things.")
 
 
 if __name__ == "__main__":
