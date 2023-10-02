669dee9d174db19d1de9bb5a9e396ffb00f6f7fe
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-10-02 13:19:58 +0200

    submission Bite 12 @ codechalleng.es

--- 12/validate.py
+++ 12/validate.py
@@ -9,8 +9,26 @@ bob = User(name="Bob", role=USER, expired=True)
 pybites = User(name="PyBites", role=ADMIN, expired=False)
 USERS = (julian, bob, pybites)
 
-# define exception classes here
+
+class UserDoesNotExist(Exception):
+    pass
+
+
+class UserAccessExpired(Exception):
+    pass
+
+
+class UserNoPermission(Exception):
+    pass
 
 
 def get_secret_token(username):
-    pass
+    for user in USERS:
+        if user.name == username:
+            if user.expired:
+                raise UserAccessExpired(f"{username}'s access has expired.")
+            elif user.role != ADMIN:
+                raise UserNoPermission(f"{username} does not have the ADMIN role.")
+            else:
+                return SECRET
+    raise UserDoesNotExist(f"{username} does not exist.")
