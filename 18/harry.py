9269083ccfc86f758a773a498abc7168b889c546
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-10-04 10:47:40 +0200

    submission Bite 18 @ codechalleng.es

--- 18/harry.py
+++ 18/harry.py
@@ -1,3 +1,4 @@
+import collections
 import os
 import urllib.request
 
@@ -12,6 +13,21 @@ urllib.request.urlretrieve(
     "https://bites-data.s3.us-east-2.amazonaws.com/harry.txt", harry_text
 )
 
+with open(harry_text, mode="r", encoding="utf-8") as file:
+    harry = file.read().lower().split()
+
+with open(stopwords_file, mode="r", encoding="utf-8") as file:
+    stopwords = file.read().split()
+
 
 def get_harry_most_common_word():
-    pass
+    cleaned_words = ["".join(filter(str.isalnum, word)) for word in harry]
+
+    non_stopwords = [
+        word for word in cleaned_words if word != "" and word not in stopwords
+    ]
+
+    return collections.Counter(non_stopwords).most_common(1)[0]
+
+
+get_harry_most_common_word()
