73d14abadee9e0a7fad365d3a50549f79d4c8770
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-10-17 11:04:09 +0200

    submission Bite 36 @ codechalleng.es

--- 36/profile.py
+++ 36/profile.py
@@ -1,2 +1,15 @@
-def get_profile():
-    pass
+def get_profile(name, age, *sports, **awards):
+    if not isinstance(age, int):
+        raise ValueError("Age must be an integer.")
+    if len(sports) > 5:
+        raise ValueError("Only up to 5 sports are allowed.")
+
+    profile = {"name": name, "age": age}
+
+    if sports:
+        profile["sports"] = list(sorted(sports))
+
+    if awards:
+        profile["awards"] = awards
+
+    return profile
