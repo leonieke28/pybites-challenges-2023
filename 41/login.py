060a4d981fa6e7b169cd03052bc9f61c8264af24
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-10-23 11:21:20 +0200

    submission Bite 41 @ codechalleng.es

--- 41/login.py
+++ 41/login.py
@@ -1,12 +1,23 @@
+import functools
+
 known_users = ["bob", "julian", "mike", "carmen", "sue"]
 loggedin_users = ["mike", "sue"]
 
 
 def login_required(func):
-    pass
+    @functools.wraps(func)
+    def wrapper_login_required(user):
+        if user not in known_users:
+            return func("please create an account")
+        elif user in known_users and user not in loggedin_users:
+            return func("please login")
+        elif user in known_users and user in loggedin_users:
+            return func(f"welcome back {user}")
+
+    return wrapper_login_required
 
 
 @login_required
 def welcome(user):
     """Return a welcome message if logged in"""
-    pass
+    return user
